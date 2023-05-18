828ddbb2e8a376f79636d023dfc2af39ce661170
n4j1Br4ch1D <kawner93@gmail.com>
2023-05-18 16:34:12 +0100

    fix #13 :Tailwindcss Advanced Classes: 1-parentheses removed

--- utils.js
+++ utils.js
@@ -193,7 +193,6 @@ function getClassNames(selectorStr) {
   const tempClasses = selectorStr
     .replace(/\.(?=[0-9-])/g, escpdSlctrPlchldr) // Match escaped dot
     .replace("::", " ")
-    .replace(/\([^\)]*\)/g, "") // Remove string between starts with ( end with )
     .replace( /(?<!\\)\([^)]*\)/g, "") // Remove string between starts with ( not'\(' end with ) not -- issue #13
     .replace(/(?<!-\\)\[[^\]]*=[^\]]*]/g, "")  // Only removes  CSS [attribute*=value] selectors when the `[` is not preceded by `-\`. thus it avoids tailwind`s arbitrary values, & data attributes
     .split(".")
